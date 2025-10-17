27f96ca99ab19c8c02bad9b2164ad45e913462c6
MichStew <stew.mich258@gmail.com>
2025-10-17 11:50:27 -0400

    add changes to simtop

--- Lab 3 CPU Design/src/simtop.sv
+++ Lab 3 CPU Design/src/simtop.sv
@@ -55,3 +55,40 @@ module simtop;
         assign SW = 18'd12345;
 
 endmodule
+
+// need to update actual architecture to reflect the cpu but this is good pseudo code 
+Initial begin 
+// instantiate our testbench, needs to check the output of the cpu. binary numbers need to output decimal numbers, really just check those 
+//set switches to desired numbers to check 
+SW = 18'h1dddd;  // this is 18 bits that, we are assigning 4 to every 4 bit section in the 18, we will check the 
+
+// need to take the input from the risc v code and read after some number of cyclces to check if we are correct 
+#20
+if (HEX0 !== 0xf00) begin 
+	$display("yo junk is cooked for //initial number) 
+end else $display("looking good for TEST CASE) 
+if (HEX1 !-- 0xf00)
+	$display("not working for input 4") 
+end else $display("looking good for 4")
+end 
+//testing, needs to be longer 
+
+// make it clock constantly
+// display statements inside the cpu to output after instruction 
+// chech csrrw with the output 
+// always_ff @ (posedge clk) 
+// if (rst) begin 
+// instruction_EX <= 32'b0;
+//PC_FETCH <= instruction_mem[PC_FETCH];
+//end 
+
+
+
+
+
+
+
+
+
+
+
